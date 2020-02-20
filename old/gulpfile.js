5e11af443b258ee0f2311a8239ab25e099237d98
AhmedMohamedSaleh <43453197+AhmedMohamedSaleh@users.noreply.github.com>
2020-02-20 09:11:25 +0200

    finish Html minify

--- old/gulpfile.js
+++ old/gulpfile.js
@@ -11,15 +11,29 @@ sass.compiler = require('node-sass');
 // minify css 
 const cssnano = require('gulp-cssnano');
 
-gulp.task('sass', () => {
+// minify html
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('sass_and_minify_css', () => {
     return gulp.src('./sass/*.scss')
         .pipe(sass())
-        //.pipe(cssnano())
+        .pipe(cssnano())
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch', () => {
-    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+gulp.task('minify_html', () => {
+    return gulp.src('*.html')
+        
+        .pipe(htmlmin({ 
+            collapseWhitespace: true,
+            removeComments: true,
+            removeAttributeQuotes: true
+        }))
+        .pipe(gulp.dest('./html'));
 });
 
-gulp.task('default', gulp.series('sass', 'watch'));
+gulp.task('watch', () => {
+    gulp.watch('./sass/**/*.scss', gulp.series('sass_and_minify_css'));
+});
+
+gulp.task('default', gulp.series('sass_and_minify_css','minify_html', 'watch'));
