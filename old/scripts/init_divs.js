8d42fb17e3219ea3326f206ad2446f54351896df
AhmedMohamedSaleh <43453197+AhmedMohamedSaleh@users.noreply.github.com>
2020-03-02 02:52:48 +0200

    general_hr_reduction

--- old/scripts/init_divs.js
+++ old/scripts/init_divs.js
@@ -4,23 +4,29 @@ window.addEventListener('DOMContentLoaded', false);
 
 function yourFunction () {
     
-// Get the element
-var elem = document.querySelector('.stars_block');
+copy_div('.stars_block' , '.stars_block_redunant');
+copy_div('.general_hr_style_underline' , '.general_hr_style_underline_redunant');
+copy_div('.our_reviews_grid_items_stars' , '.our_reviews_grid_items_stars_redunant');
 
-// Create a copy of it
-var clone = elem.cloneNode(true);
+function copy_div(original_div_name,coped_div_name){
 
-// Update the ID and add a class
-clone.id = 'stars_block_redunant';
+    // Get the element
+    var elem = document.querySelector(original_div_name);
 
-var elements = document.querySelectorAll('.stars_block_redunant');
+    // Create a copy of it
+    var clone = elem.cloneNode(true);
 
-for(var i=0;i<elements.length;i++)
-{        
-        // Create a copy of it
-        var clone2 = clone.cloneNode(true);
-        elements[i].append(clone2);
+    // Update the ID and add a class
+    clone.id = coped_div_name;
+
+    var elements = document.querySelectorAll(coped_div_name);
+
+    for(var i=0;i<elements.length;i++)
+    {        
+            // Create a copy of it
+            var clone2 = clone.cloneNode(true);
+            elements[i].append(clone2);
+    }
 }
-
 
 }
